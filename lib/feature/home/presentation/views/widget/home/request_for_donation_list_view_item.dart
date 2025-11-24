54b9aa96f36ce8dc3e29397518ddbce6b4c83a9e
amira <amira.mostafa123455@gmail.com>
2025-11-24 17:35:32 +0200

    change all requests screen ui

--- lib/feature/home/presentation/views/widget/home/request_for_donation_list_view_item.dart
+++ lib/feature/home/presentation/views/widget/home/request_for_donation_list_view_item.dart
@@ -1,6 +1,5 @@
 import 'package:blood_bank/core/utils/app_text_style.dart';
 import 'package:blood_bank/core/utils/assets_images.dart';
-import 'package:blood_bank/core/widget/coustom_circular_progress_indicator.dart';
 import 'package:blood_bank/feature/localization/app_localizations.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
--- lib/feature/home/presentation/views/widget/see_all.dart
+++ lib/feature/home/presentation/views/widget/see_all.dart
@@ -5,7 +5,6 @@ import 'package:blood_bank/core/widget/coustom_dialog.dart';
 import 'package:blood_bank/feature/home/presentation/views/donor_details.dart';
 import 'package:blood_bank/feature/home/presentation/views/widget/home/request_for_donation_list_view_item.dart';
 import 'package:blood_bank/feature/localization/app_localizations.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -59,83 +58,11 @@ class SeeAllScreen extends StatelessWidget {
             },
             child: RequestForDonationListViewItem(request: request));
 
-        //   ListTile(
-        //   leading: _buildUserAvatar(context, request['uId']),
-        //   title: Text(request['name'] ?? 'No Name'),
-        //   subtitle: _buildSubtitle(request, context),
-        //   trailing: _buildTrailing(request, context),
-        //   onTap: () {
-        //     Navigator.of(context).push(
-        //       buildPageRoute(
-        //           DonorProfileScreen(uId: request['uId'])
-        //       ),
-        //     );
-        //
-        //   },
-        // );
-
       },
     );
   }
 
 
-  // Widget _buildUserAvatar(BuildContext context, String userId) {
-  //   return FutureBuilder<DocumentSnapshot>(
-  //     future: FirebaseFirestore.instance.collection('users').doc(userId).get(),
-  //     builder: (context, snapshot) {
-  //       if (snapshot.connectionState == ConnectionState.waiting) {
-  //         return const CircleAvatar(
-  //           radius: 25,
-  //           backgroundColor: Colors.grey,
-  //           child: Icon(Icons.person, color: Colors.white),
-  //         );
-  //       }
-  //
-  //       if (snapshot.hasError || !snapshot.hasData || !snapshot.data!.exists) {
-  //         return const CircleAvatar(
-  //           radius: 25,
-  //           backgroundColor: Colors.grey,
-  //           child: Icon(Icons.person, color: Colors.white),
-  //         );
-  //       }
-  //
-  //       final userData = snapshot.data!.data() as Map<String, dynamic>;
-  //       final photoUrl = userData['photoUrl'];
-  //
-  //       if (photoUrl == null || photoUrl.isEmpty) {
-  //         return const CircleAvatar(
-  //           radius: 25,
-  //           backgroundColor: Colors.grey,
-  //           child: Icon(Icons.person, color: Colors.white),
-  //         );
-  //       }
-  //
-  //       return CircleAvatar(
-  //         radius: 25,
-  //         backgroundColor: Colors.grey,
-  //         child: ClipOval(
-  //           child: CachedNetworkImage(
-  //             imageUrl: photoUrl,
-  //             width: 50,
-  //             height: 50,
-  //             fit: BoxFit.cover,
-  //             placeholder: (context, url) => const SizedBox(),
-  //             errorWidget: (context, url, error) => const Icon(Icons.person, color: Colors.white),
-  //           ),
-  //         ),
-  //       );
-  //     },
-  //   );
-  // }
-  //
-  // Widget _buildSubtitle(Map<String, dynamic> request, BuildContext context) {
-  //   return Text(
-  //     '${'blood_types'.tr(context)}: ${request['bloodType'].toString().tr(context)}',
-  //   );
-  // }
-  //
-  // Widget _buildTrailing(Map<String, dynamic> request, BuildContext context) {
-  //   return Text('${request['distance'] ?? '0'} ${'km'.tr(context)}');
-  // }
+
 }
 
