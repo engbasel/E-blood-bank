8f653bc66e1d4988351706b22538616f49075d77
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-11-30 07:07:36 +0200

    create user chat model

---
+++ lib/feature/chat/data/models/user_chat_model.dart
@@ -0,0 +1,35 @@
+class UserChatModel {
+  final String userId;
+  final String name;
+  final String imageUrl;
+
+  final String lastMessage;
+  final DateTime? lastMessageTime;
+  final bool lastMessageSeen;
+  final String lastMessageSenderId;
+
+  UserChatModel({
+    required this.userId,
+    required this.name,
+    required this.imageUrl,
+    required this.lastMessage,
+    required this.lastMessageTime,
+    required this.lastMessageSeen,
+    required this.lastMessageSenderId,
+  });
+
+  factory UserChatModel.fromData({
+    required Map<String, dynamic> userData,
+    required Map<String, dynamic>? chatData,
+  }) {
+    return UserChatModel(
+      userId: userData["uid"],
+      name: userData["name"] ?? "",
+      imageUrl: userData["imageUrl"] ?? "",
+      lastMessage: chatData?["lastMessage"] ?? "",
+      lastMessageTime: chatData?["lastMessageTime"]?.toDate(),
+      lastMessageSeen: chatData?["lastMessageSeen"] ?? true,
+      lastMessageSenderId: chatData?["lastMessageSenderId"] ?? "",
+    );
+  }
+}
